a1366085ef5d9ffb15ba32573b76b52a43fbfd1d
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-04-11 16:51:45 +0200

    Reformate previous code

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
 $(function(){
-				var todo = document.getElementById('todo');
-				var clear = document.getElementById('clear');
+	var todo = document.getElementById('todo');
+	var clear = document.getElementById('clear');
 
-				// Stocker le contenu de la liste
-				$('#todo').blur(function(){
-					localStorage.setItem('data', this.innerHTML);
-				});
+	// Stocker le contenu de la liste
+	$('#todo').blur(function(){
+		localStorage.setItem('data', this.innerHTML);
+	});
 
 
 
-				// Restaure le contenu dans le code HTML
-				if (localStorage.getItem('data')){
-					todo.innerHTML = localStorage.getItem('data');
-				}
-				// Réinitialiser l'espace de stockage
-				$(function(){
-					$('#clear').click(function(){
-						localStorage.clear();
-						location.reload();
-					});
-				});
+	// Restaure le contenu dans le code HTML
+	if (localStorage.getItem('data')){
+		todo.innerHTML = localStorage.getItem('data');
+	}
+	// Réinitialiser l'espace de stockage
+	$(function(){
+		$('#clear').click(function(){
+			localStorage.clear();
+			location.reload();
+		});
+	});
 
-				// Ajout de classes de personnalisation
-				function couleur (){
-					$("li:contains('!')").addClass('prioritaire');
-					$("li:contains('ok')").addClass('fait');
-					$("li:not(:contains('!'))").removeClass('prioritaire');
-					$("li:not(:contains('ok'))").removeClass('fait');
-				};
-				
-				$('#todo').keyup(function (){
-					couleur();
-				})
+	// Ajout de classes de personnalisation
+	function couleur (){
+		$("li:contains('!')").addClass('prioritaire');
+		$("li:contains('ok')").addClass('fait');
+		$("li:not(:contains('!'))").removeClass('prioritaire');
+		$("li:not(:contains('ok'))").removeClass('fait');
+	};
+	
+	$('#todo').keyup(function (){
+		couleur();
+	})
 
-			});
+});
