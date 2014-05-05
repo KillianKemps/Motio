2212963778557cfe0166928f2ba9362b119fad69
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-05-05 19:25:11 +0200

    Correct bug with edit button

--- js/main.js
+++ js/main.js
@@ -26,19 +26,35 @@ $(function(){
 
 	//Add new task
 	$('#add-task').click(function(){
-		var $task = $("<li><input type=\"checkbox\"/> <p contenteditable=\"true\">To do</p></li>")
-			.append($("<button type=\"button\" id=\"edit-task\" contenteditable=\"false\">Edit</button>")
-				.click(function(){
+		var $task = $("<li><p contenteditable=\"true\">To do</p></li>");
+		var $taskCheckbox = $("<input type=\"checkbox\"/>").on('click', function(){
+						$(this).parent().detach();
+					if ( $(this).is( ":checked" ) ){
+						$(this).parent().appendTo("#completed-tasks");
+						$(this).attr( "checked", true );
+					}
+					else{
+						$(this).parent().appendTo("#todo");
+						$(this).attr( "checked", false );	
+					}
+					
+					localStorage.setItem('completed-tasks-data', completedTasks.innerHTML);
+					localStorage.setItem('todo-data', todo.innerHTML);
+				});
+
+		$($taskCheckbox).prependTo($task);
+				
+		var $taskButton = $("<button type=\"button\" class=\"edit-task\" contenteditable=\"false\">Edit</button>").click(function(){
 						$( "#item-dialog" ).dialog();
-					}));
+					});
+		$($task).append($taskButton);
+				
 		$( "#todo" ).append($task);
 	});
 
 	/*Open Popup on click on item*/
-		$('#edit-task').on('click', function(){
-
+		$('.edit-task').on('click', function(){
 			$( "#item-dialog" ).dialog();
-
 		});
 
 		/* Remove checked task */
