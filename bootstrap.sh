c0e9ea004afb1517713ae72f955d5e4759b94e07
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-05-26 22:30:13 +0200

    Changed shell script for Vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,9 @@
 apt-get update
 apt-get install -y nodejs npm 
 cd /vagrant
-echo "---npm install"
-npm install
-echo "---bower install"
-bower install
+#echo "---npm install"
+#npm install
+#echo "---bower install"
+#bower install
 echo "---Running Server on 192.168.33.10"
 node server.js
