7be7ad213261bb69ec566afde793ec4184b275b5
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-05-26 22:18:06 +0200

    Add Vagrantfile

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y nodejs npm 
+cd /vagrant
+echo "---npm install"
+npm install
+echo "---bower install"
+bower install
+echo "---Running Server on 192.168.33.10"
+node server.js
