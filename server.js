0906208b2abe58a483b7a54186931e5aea4644ba
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-05-29 18:44:43 +0200

    Add node files to connect to MongoDB

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ var mongoose= require('mongoose');
 var db = require('./config/db');
 
 var port = process.env.PORT || 8080; // set our port
-// mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
+mongoose.connect(db.url, function(err, db) {
+  if(!err) {
+    console.log("We are connected to mongoDB");
+  }
+}); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
 app.configure(function() {
 	app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
