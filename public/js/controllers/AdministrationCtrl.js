9762f2fe84f56c6c6dd1cedb367cd04feb3a11cb
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-06-06 23:37:42 +0200

    Changed administration layout. Added dynamic content for other parameters too

--- public/js/controllers/AdministrationCtrl.js
+++ public/js/controllers/AdministrationCtrl.js
@@ -1,5 +1,22 @@
-angular.module('AdministrationCtrl', []).controller('AdministrationController', function($scope) {
+var administrationController = angular.module('AdministrationCtrl', []);
+
+administrationController.controller('AdministrationController', function($scope) {
 
 	$scope.tagline = 'Here\'s where you will be able to change parameters !';
+
+	$scope.pseudo = 'John Doe';
+
+	$scope.email = 'myemail@home.test';
+
+	$scope.modulelist = [
+    {'name': 'Module 1',
+     'snippet': 'One description'},
+    {'name': 'Module 2',
+     'snippet': 'The second description'},
+    {'name': 'Module 3',
+     'snippet': 'Another description'}
+  	];
+
+  	$scope.kernelversion = '0.1.0';
 
 });
