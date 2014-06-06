774c965c61f0ef9dc4cc4a8891d71a80bb341310
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-06-06 23:45:13 +0200

    Added the last caracteristics of modules

--- public/js/controllers/AdministrationCtrl.js
+++ public/js/controllers/AdministrationCtrl.js
@@ -10,10 +10,19 @@ administrationController.controller('AdministrationController', function($scope)
 
 	$scope.modulelist = [
     {'name': 'Module 1',
+     'version': '1.2.3',
+     'author': 'Eddy Doe',
+     'website': 'http://testwebsite.fr',
      'snippet': 'One description'},
     {'name': 'Module 2',
+     'version': '1.2.3',
+     'author': 'Eddy Doe',
+     'website': 'http://testwebsite.fr',
      'snippet': 'The second description'},
     {'name': 'Module 3',
+     'version': '1.2.3',
+     'author': 'Eddy Doe',
+     'website': 'http://testwebsite.fr',
      'snippet': 'Another description'}
   	];
 
