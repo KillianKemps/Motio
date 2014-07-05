bffdceba018b74dd9b2df01f22f031ce623a7dcf
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-07-05 15:19:12 +0200

    Able to add task and to check them

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -2,4 +2,14 @@ angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
 	$scope.tagline = 'Your Future is created by what you do today not tomorrow';	
 
+	$scope.items = [
+		{text:  'task 1', done:'false'},
+		{text:  'task 2', done:'true'},
+		{text:  'task 3', done:'true'}
+	];
+
+	$scope.addTask = function(){
+		var item = {text: $scope.formTodoText, done: false};
+		$scope.items.push(item);
+	}
 });
