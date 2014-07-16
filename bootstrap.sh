9c68aad38adc53a210c117dc8181944f073fb463
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-07-16 20:51:33 +0200

    Updated bootsrap.sh and removed unused packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,16 @@
 #Install MongoDB
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:chris-lea/node.js
+#sudo apt-get install python-software-properties
+#sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
-sudo apt-get install nodejs
+#sudo apt-get install nodejs
+apt-get install -y nodejs npm mongodb-org
 
 cd /vagrant
 sudo npm -g install karma
 sudo npm install --save-dev karma-chai
-#apt-get install -y nodejs npm mongodb-org
+
 
 
 #echo "---npm install"
