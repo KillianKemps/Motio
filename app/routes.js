2c27c8ae2a4a0afec83bff86b626b562699b16a3
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-07-27 18:29:33 +0200

    Add sign Up

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,30 @@
 module.exports = function(app) {
 
+	/**************/
+	/** Todo API **/
+	/**************/
 	var todo = require('./models/todo');
 
 	app.param('todoId', todo.todo);
  
-	
 	app.get('/api/todo', todo.all);
 	app.get('/api/todo/:todoId', todo.show)
 	app.post('/api/todo', todo.create);
 	app.put('/api/todo/:todoId', todo.update);
 	app.del('/api/todo/:todoId', todo.remove);
+
+	/*****************/
+	/** Sign Up API **/
+	/*****************/
+
+	var user = require('./models/user.js');
+	var passport = require('passport');
+
+	// process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+	}));
 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // modules =================================================
-var express = require('express');
-var app     = express();
-var mongoose= require('mongoose');
+var express 	= require('express');
+var app     	= express();
+var mongoose	= require('mongoose');
+var passport 	= require('passport');
+var session     = require('express-session');
 
 // configuration ===========================================
 	
@@ -60,10 +62,19 @@ app.configure(function() {
 	app.use(express.logger('dev')); 					// log every request to the console
 	app.use(express.bodyParser()); 						// pull information from html in POST
 	app.use(express.methodOverride()); 					// simulate DELETE and PUT
+	
+	// required for passport
+	app.use(session({ 
+		secret: 'thisisasessionsecret',
+		saveUninitialized: true,
+        resave: true })); // session secret
+	app.use(passport.initialize());
+	app.use(passport.session()); // persistent login sessions
 });
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
+require('./config/passport')(passport); // pass passport for configuration
 
 // start app ===============================================
 app.listen(port);	
