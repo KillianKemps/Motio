880c103db60faad64c47e5b3ff40011b8f77034d
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2014-08-15 17:43:45 +0200

    Optimized Mongo connection

--- server.js
+++ server.js
@@ -18,18 +18,9 @@ var dbconfig = require('./config/db');
 
 var port = process.env.PORT || 8080; // set our port
 
-var mongo = require('mongodb');
-
 var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mydb';
-
-mongo.Db.connect(mongoUri, function (err, db) {
-  /*db.collection('mydocs', function(er, collection) {
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-    });
-  });*/
-});
+  'mongodb://localhost/motivatemedb';
 
 mongoose.connect(mongoUri, function(err, dbconfig) {
   if(!err) {
